200531ee0737980b4b880727b669fd5e5434bd17
Alvarez <122706047+Alvarezzzzz@users.noreply.github.com>
2023-12-02 09:51:37 -0400

    Update Principal.java
    Escribi otro msj

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola mundoNOT");
+        System.out.println("Holo otra vez")
     }
     
 }
